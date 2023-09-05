251741e8396d395abddc2678fa920929c7a35542
akshetpatial <akshet.patial@gmail.com>
2023-09-05 12:27:35 +0530

    Added the minSubArraySum method

--- src/com/akshet/MinSumSubArrayFixed.java
+++ src/com/akshet/MinSumSubArrayFixed.java
@@ -4,11 +4,37 @@ import java.util.Scanner;
 
 public class MinSumSubArrayFixed {
 
+    private static int minSubArraySum(int[] arr, int n, int k) {
+        int upper_loop  = 0;
+        int inside_loop = 0;
+        int min_sum = Integer.MAX_VALUE;
+        int sum =0;
+        // while(upper_loop<=n-k){
+        while(inside_loop<n){
+            if(inside_loop-upper_loop+1 !=k){
+                sum+=arr[inside_loop];
+                inside_loop++;
+            }else{
+                sum+=arr[inside_loop];
+                // System.out.print("sum: "+sum);
+                if(min_sum>sum){
+                    min_sum=sum;
+                }
+                sum=sum-arr[upper_loop];
+                upper_loop++;
+                inside_loop++;
+                // inside_loop=upper_loop;
+            }
+            // }
+        }
+        return min_sum;
+    }
+
     public static void main(String[] args) {
         System.out.println("Enter the Size of the Array: ");
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
-        System.out.println("Enter the elements of the Array: ");
+        System.out.println("Enter the elements of the Array: "); // 10 4 2 5 6 3 8 1
         int[] arr = new int[size];
         for(int i =0;i<size;i++)
             arr[i] = scan.nextInt();
@@ -16,7 +42,7 @@ public class MinSumSubArrayFixed {
         System.out.print("[");
         for(int i =0;i<size;i++)
             System.out.print(arr[i]+",");
-        System.out.print("]");
+        System.out.println("]");
         System.out.println("Enter the Size of the window: ");
         int win = scan.nextInt();
 
